200cec45d38885cbe4dfc5762998587da3fe0534
Danial Saleemi <salimicse@gmail.com>
2024-02-14 10:16:04 +0500

    Added loading states, modified baseURL

--- src/components/taskcomponents.tsx
+++ src/components/taskcomponents.tsx
@@ -7,15 +7,16 @@ import { UUID } from "crypto";
 import { BiLoader, BiRefresh } from "react-icons/bi";
 import Image from "next/image";
 import Link from "next/link";
+import AddTask from "./addtaskform";
 
-interface TaskItem {
+export interface TaskItem {
   [x: string]: any;
   id: UUID;
   title: string;
   completed: boolean;
 }
 
-const URL = process.env.NEXT_PUBLIC_VERCEL_URL
+export const BaseURL = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api` ||
     `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}/api`
   : "http://localhost:3000/api";
@@ -24,33 +25,22 @@ const URL = process.env.NEXT_PUBLIC_VERCEL_URL
 
 const TaskComponents = () => {
   const [todos, setTodos] = useState<TaskItem[]>([]);
-  const [newTodo, setNewTodo] = useState("");
   const [todoCompleted, setTodoCompleted] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  // Fetch ToDo items on component mount
   useEffect(() => {
     fetchTodos();
   }, []);
 
   const fetchTodos = () => {
-    // Fetch ToDo items on component mount
-
+    setIsLoading(true);
     axios
-      // .get(`${URL}/items`, { withCredentials: true})
-      .get(`${URL}/items`)
+      .get(`${BaseURL}/items`)
       .then((response) => setTodos(response.data))
-      .catch((error) => console.error(error));
+      .catch((error) => console.error(error))
+      .finally(() => setIsLoading(false));
   };
 
-  const addTodo = () => {
-    axios
-      .post<TaskItem>(`${URL}/items`, { title: newTodo })
-      .then((response) => {
-        console.log(response);
-        setTodos([...todos, response.data]);
-        setNewTodo("");
-      })
-      .catch((error) => console.error(error));
-  };
 
   const updateTodo = (id: UUID) => {
     // Update the state to reflect loading for the specific task being updated
@@ -77,7 +67,7 @@ const TaskComponents = () => {
     };
 
     axios
-      .patch<TaskItem>(`${URL}/items/${id}`, updateData)
+      .patch<TaskItem>(`${BaseURL}/items/${id}`, updateData)
       .then((response) => {
         setTodos(
           todos.map((todo) =>
@@ -94,7 +84,7 @@ const TaskComponents = () => {
   };
   const deleteTodo = (id: UUID) => {
     axios
-      .delete(`${URL}/items/${id}`)
+      .delete(`${BaseURL}/items/${id}`)
       .then(() => {
         setTodos(todos.filter((todo) => todo.id !== id));
       })
@@ -104,7 +94,7 @@ const TaskComponents = () => {
   // delete all todo items
   const deleteAllTodos = () => {
     axios
-      .delete(`${URL}/items`)
+      .delete(`${BaseURL}/items`)
       .then(() => {
         setTodos([]);
       })
@@ -142,20 +132,9 @@ const TaskComponents = () => {
           />
         </Link>
       </div>
-      {/* <input
-        name="task title"
-        id="task1"
-        type="text"
-        value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
-        className="bg-white border-2 border-gray-200 rounded w-4/6 py-4 text-gray-700 text-lg leading-tight focus:outline-none focus:bg-white focus:border-purple-500/40"
-      />
-      <button
-        className="bg-[#43766C] hover:ring-1 text-white text-lg font-semibold p-2 rounded"
-        onClick={addTodo}
-      >
-        Add Item
-      </button> */}
+      
+      <AddTask/>
+
       <div className="flex flex-col ">
         <div className=" space-y-4 py-6">
           {todos
@@ -166,18 +145,19 @@ const TaskComponents = () => {
                 ? 1
                 : -1;
             })
+
             .map((todo) => (
               <div key={todo.index} className="flex space-x-6 items-center">
-                <div
-                  className={`flex basis-10/12 bg-gradient-to-l from-slate-300/30 to-slate-100/10 border-blue-200/20 text-lg text-[#333333] shadow-md border-2 rounded-md py-6 px-2 ${
-                    todo.completed
-                      ? "text-green-800 bg-green-400/50 line-through font-light"
-                      : "font-normal"
-                  }`}
-                >
-                  {todo.title}
-                </div>
-
+                  <div
+                    className={`flex basis-10/12 bg-gradient-to-l from-slate-300/30 to-slate-100/10 border-blue-200/20 text-lg text-[#333333] shadow-md border-2 rounded-md py-6 px-2 ${
+                      todo.completed
+                        ? "text-green-800 bg-green-400/50 line-through font-light"
+                        : "font-normal"
+                    }`}
+                  >
+                    {todo.title}
+                  </div>
+                
                 {!todo.isLoading ? (
                   <input
                     title="Mark completed/incomplete"
@@ -189,11 +169,18 @@ const TaskComponents = () => {
                 ) : (
                   <BiLoader size={30} />
                 )}
-                <MdDelete title="Remove this task" size={30} onClick={() => deleteTodo(todo.id)} />
+                <MdDelete
+                  title="Remove this task"
+                  size={30}
+                  onClick={() => deleteTodo(todo.id)}
+                />
               </div>
             ))}
         </div>
       </div>
+      {isLoading && (
+        <p className="text-xl font-bold text-center w-full text-indigo-700 animate-pulse my-8">Loading...</p>
+      )}
       <button
         className="bg-[#3f7cc2d4] hover:ring-1 text-white text-lg font-semibold p-2 rounded"
         onClick={deleteAllTodos}
