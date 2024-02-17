6bf7375908f98e6bdeb26ac08f0f51f7365bb32f
Danial Saleemi <salimicse@gmail.com>
2024-02-17 23:27:15 +0500

    Axios related code moved to crud.ts

--- src/utils/crud.ts
+++ src/utils/crud.ts
@@ -3,8 +3,8 @@ import { BaseURL, TaskItem } from '@/components/taskcomponents';
 import { UUID } from 'crypto';
 
 
-export const fetchItems = () => axios.get(`${BaseURL}/items`)
-export const addItem = (newTodo : string) => axios.post(`${BaseURL}/items`, { title : newTodo })
-export const updateItem = (id : UUID, updatedTodo : TaskItem) => axios.patch(`${BaseURL}/items/${id}`, updatedTodo)
-export const deleteItem = (id : UUID) => axios.delete(`${BaseURL}/items/${id}`)
-export const deleteAllItems = () => axios.delete(`${BaseURL}/items`)
+export const fetchItems = async () => axios.get(`${BaseURL}/items`)
+export const addItem = async (newTodo : string) => axios.post(`${BaseURL}/items`, { title : newTodo })
+export const updateItem = async (id : UUID, updatedTodo : TaskItem) => axios.patch(`${BaseURL}/items/${id}`, updatedTodo)
+export const deleteItem = async (id : UUID) => axios.delete(`${BaseURL}/items/${id}`)
+export const deleteAllItems = async () => axios.delete(`${BaseURL}/items`)
