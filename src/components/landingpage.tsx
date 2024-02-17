796258fe335cca6b1e618fea9f25687346b9b0e0
Danial Saleemi <salimicse@gmail.com>
2024-02-17 23:44:44 +0500

    Added GPT Link button

--- src/components/landingpage.tsx
+++ src/components/landingpage.tsx
@@ -11,32 +11,35 @@ const LandingPage = () => {
         <Carousel />
       </div>
       <div className="flex items-center justify-center space-x-2">
-        <Link
-          href={"https://chat.openai.com/g/g-pRFPHnbZb-taskgenius"}
-          target="_blank"
-          title="Open custom ChatGPT interface for Task Genius"
-        >
-          <div className="flex flex-col items-center gap-6 mt-2">
-            <Image
-              src="/logo.png"
-              alt="logo"
-              loading="lazy"
-              style={{
-                width: "15%",
-                height: "auto",
-              }}
-              width={75}
-              height={75}
-            />
-            <h1 className="text-3xl lg:text-6xl font-extrabold text-[#3e2890] text-opacity-70 tracking-wider">
-              Task Genius AI
-            </h1>
-            <p className="font-extralight text-violet-800">
-              Generative Genius: Transform Your Daily activities with AI-Driven
-              Action!
-            </p>
-          </div>
-        </Link>
+        <div className="flex flex-col items-center gap-6 mt-2">
+          <Link
+            href={"https://chat.openai.com/g/g-pRFPHnbZb-taskgenius"}
+            target="_blank"
+            title="Open custom ChatGPT interface for Task Genius"
+          >
+            <div className="flex items-center justify-center text-sm text-violet-900 text-opacity-60 hover:text-opacity-100 hover:scale-110 transition-transform font-extralight tracking-tighter">
+              Access TaskGenius GPT here 👉
+              <Image
+                src="/logo.png"
+                alt="logo"
+                loading="lazy"
+                style={{
+                  width: "15%",
+                  height: "auto",
+                }}
+                width={75}
+                height={75}
+              />
+            </div>
+          </Link>
+          <h1 className="text-3xl lg:text-6xl font-extrabold text-violet-700 text-opacity-70 tracking-wider">
+            Task Genius AI
+          </h1>
+          <p className="font-extralight text-violet-800">
+            Generative Genius: Transform Your Daily activities with GPT-Driven
+            Actions!
+          </p>
+        </div>
       </div>
       <TaskComponents />
     </div>
