b9a4b897ab62e5dc6c90379c4542520ccb7282bb
Danial Saleemi <salimicse@gmail.com>
2024-02-20 10:52:27 +0500

    some basic ui modifications

--- src/components/stackcarousel.tsx
+++ src/components/stackcarousel.tsx
@@ -29,37 +29,43 @@ const Carousel = () => {
     "/vercel.svg",
   ];
   return (
-    <div className="flex items-center min-h-[80px]">
+    <div
+      className="flex items-center min-h-[80px]"
+      title="Technology stack used to build this application"
+    >
       <div className="relative overflow-hidden">
         <div className="absolute py-6 inset-0 z-5 items-center">
-          <p className="text-center items-text-xl md:text-2xl lg:text-3xl text-slate-400/70 font-bold tracking-widest">
+          <p className="text-center text-3xl lg:text-6xl text-slate-400/30 md:text-slate-400/10 font-bold tracking-widest">
             Technology Stack
           </p>
         </div>
-          <div className="whitespace-nowrap animate-slide flex items-center z-20">
-            {items.map((item, index) => (
-              <div key={index} className="inline-flex py-2 px-4 min-h-[80px] min-w-[80px]">
-                <Image
-                  src={item}
-                  alt={`Tech ${index + 1}`}
-                  width={200}
-                  height={200}
-                />
-              </div>
-            ))}
-            {items.map((item, index) => (
-              <div
-                key={`copy-${index}`}
-                className="inline-flex py-2 px-4 min-h-[80px] min-w-[80px]"
-              >
-                <Image
-                  src={item}
-                  alt={`Tech ${index + 1}`}
-                  width={200}
-                  height={200}
-                />
-              </div>
-            ))}
+        <div className="whitespace-nowrap animate-slide flex items-center z-20">
+          {items.map((item, index) => (
+            <div
+              key={index}
+              className="inline-flex py-2 px-4 min-h-[80px] min-w-[80px]"
+            >
+              <Image
+                src={item}
+                alt={`Tech ${index + 1}`}
+                width={200}
+                height={200}
+              />
+            </div>
+          ))}
+          {items.map((item, index) => (
+            <div
+              key={`copy-${index}`}
+              className="inline-flex py-2 px-4 min-h-[80px] min-w-[80px]"
+            >
+              <Image
+                src={item}
+                alt={`Tech ${index + 1}`}
+                width={200}
+                height={200}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
